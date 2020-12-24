f4cae2b25d5480f7f57b6170232b4b43b728efcc
Al Fian <al.fian.v001@gmail.com>
2020-12-24 13:10:46 +0700

    Added Bond model and all other modefied files

--- db/migrate/20201224052906_create_users.rb
+++ db/migrate/20201224052906_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :email, null: false
-      t.boolean :is_public, null: false, default: true 
+      t.boolean :is_public, null: false, default: true
 
       t.timestamps
     end
