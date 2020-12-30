07d3eb390053043201f965be9bd3705a81db03a9
Al Fian <al.fian.v001@gmail.com>
2020-12-30 13:08:05 +0700

    Reinstalled Tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import '../css/application'
+import '../css/application.css'
 
 Rails.start()
 Turbolinks.start()
