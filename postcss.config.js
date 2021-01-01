fe5ac04c81bafa78527a787d4f095983af58ce5c
Al Fian <al.fian.v001@gmail.com>
2021-01-01 21:41:14 +0700

    Copied over node_modules to make sure TailWindCss works

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('tailwindcss')('./app/javascript/css/tailwind.js'),
+    require('tailwindcss')('./app/javascript/tailwind.config.js'),
     require('autoprefixer'),
+    require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
